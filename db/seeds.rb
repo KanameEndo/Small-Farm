7a99d984efe08215cb4da9140e0a4ce11f1cee14
Kaname_Endo <uchiyama.uchiyama.travail@gmail.com>
2022-08-07 17:34:53 +0900

    seed作成にエラー

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,41 @@
+# User生成
 10.times do |n|
-  name = Faker::Games::Pokemon.name
+  name = Gimei.kanji
   email = Faker::Internet.email
   password = "password"
-  User.create!(name: name,
-              email: email,
-              password: password,
-              )
+  User.create!(
+    name:         name,
+    email:        email,
+    password: password,
+  )
 end
+
+# スケジュール生成
 10.times do |n|
   titile = Gimei.kanji
   content = Gimei.kanji
   start_time = Faker::Date.forward(days: 10)
   Plan.create!(
-    titile:         titile, 
-    content:         content, 
-    start_time:          start_time, 
-    user_id:           "1", 
+    titile:         titile,
+    content:         content,
+    start_time:          start_time,
+    user_id:           "2",
+    created_at:          "2021-01-01 00:00:00",
+    updated_at:          "2021-01-01 00:00:00"
+  )
+end
+
+# 食品生成
+10.times do |n|
+  item_name = Gimei.kanji
+  variety = Gimei.kanji
+  Food.create!(
+    item_name:         item_name,
+    variety:         variety,
+    storage_method:        "2週間",
+    harvest:          "8月15日〜8月20日予定",
+    price:               "200",
+    user_id:              "2",
     created_at:          "2021-01-01 00:00:00",
     updated_at:          "2021-01-01 00:00:00"
   )
