d5d9a5de37b63e135584c63be34d9ca13d78ac01
Kaname_Endo <uchiyama.uchiyama.travail@gmail.com>
2022-08-08 10:02:50 +0900

    スケジュールのコントローラー作成

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,4 +1,45 @@
 class PlansController < ApplicationController
   def index
+    @plans = Plan.all
+    @plan = Plan.new
   end
+
+  def new
+    @plan = Plan.new
+  end
+
+  def show
+    @plan = Plan.find(params[:id])
+  end
+
+  def create
+    Plan.create(plan_parameter)
+    redirect_to plans_path
+  end
+
+  def destroy
+    @plan = Plan.find(params[:id])
+    @plan.destroy
+    redirect_to plans_path, notice:"削除しました"
+  end
+
+  def edit
+    @plan = Plan.find(params[:id])
+  end
+
+  def update
+    @plan = Plan.find(params[:id])
+    if @plan.update(plan_parameter)
+      redirect_to plans_path, notice: "編集しました"
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def plan_parameter
+    params.require(:plan).permit(:title, :content, :start_time)
+  end
+
 end
