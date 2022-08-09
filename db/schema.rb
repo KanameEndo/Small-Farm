a280fedfab24601944009d614d95012754eacb0d
Kaname_Endo <uchiyama.uchiyama.travail@gmail.com>
2022-08-09 10:24:55 +0900

    非同期処理完了

--- db/schema.rb
+++ db/schema.rb
@@ -73,14 +73,14 @@ ActiveRecord::Schema.define(version: 2022_08_08_115826) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "name", default: "", null: false
-    t.boolean "admin", default: false
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name", default: "", null: false
+    t.boolean "admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
