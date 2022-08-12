8f23d0eb3aa9005befed22cea2a780b1156a02d0
Kaname_Endo <uchiyama.uchiyama.travail@gmail.com>
2022-08-12 09:14:52 +0900

    エラーだらけ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @foods = @user.foods
 
-    favorites = Favorite.where(user_id: current_user.id).pluck(:Food_id)  # ログイン中のユーザーのお気に入りのFood_idカラムを取得
+    favorites = Favorite.where(user_id: current_user.id).pluck(:food_id)  # ログイン中のユーザーのお気に入りのFood_idカラムを取得
     @favorite_list = Food.find(favorites)     # Foodsテーブルから、お気に入り登録済みのレコードを取得
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,17 @@
 Rails.application.routes.draw do
   get 'users/show'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users, controllers: { registrations: 'users/registrations' }
-    # get "users/show" => "users#show"
-  resources :users, only: [:show, :edit, :update]do
-    # resources :favorites, only: [:index]
+  devise_for :users
+  # , controllers: { registrations: 'users/registrations' }
+  resources :users, only: [:show, :edit, :update] do
     get :favorites, on: :collection
   end
-  # resources :foods, only: [:index, :new, :create, :edit, :update, :show, :destroy] do
+
   resources :foods, expect: [:index] do
     resource :favorites, only: [:create, :destroy]
   end
   post 'favorite/:id' => 'favorites#create', as: 'create_favorite'
-  # delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
 
-    #   resources :foods, only: [:index, :new, :create, :show,  :edit, :destroy]
-    # end
-  
   resources :plans
   resources :favorites, only: [:create, :destroy]
   root 'foods#index'
