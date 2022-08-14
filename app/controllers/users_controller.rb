553d5f78a8afe3965ec281e14ff0b764d25d95f2
Kaname_Endo <uchiyama.uchiyama.travail@gmail.com>
2022-08-14 14:51:32 +0900

    マイページ編集画面遷移エラー解消

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,23 @@ class UsersController < ApplicationController
     favorites = Favorite.where(user_id: current_user.id).pluck(:food_id)  # ログイン中のユーザーのお気に入りのFood_idカラムを取得
     @favorite_list = Food.find(favorites)     # Foodsテーブルから、お気に入り登録済みのレコードを取得
   end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+  params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
