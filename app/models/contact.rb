a5af9ee2281b31ddb2b590613778b6f5a353d7ca
Kaname_Endo <uchiyama.uchiyama.travail@gmail.com>
2022-08-18 12:19:00 +0900

    プロフィール編集ボタン修正OK

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,5 @@ class Contact < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :email, uniqueness: true
   before_validation { email.downcase! }
 end
