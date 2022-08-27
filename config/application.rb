cdec35763bd072791f764904fb0ec8520e8e785d
Kaname_Endo <uchiyama.uchiyama.travail@gmail.com>
2022-08-27 09:12:26 +0900

    vimを修正

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module SmallFarm
     config.i18n.default_locale = :ja
     config.active_job.queue_adapter = :delayed_job
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
