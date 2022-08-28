462829b694f7560985e7443de92c88927cf899b9
Kaname_Endo <uchiyama.uchiyama.travail@gmail.com>
2022-08-28 13:34:56 +0900

    テスト2

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -19,13 +19,13 @@ preload_app true
 
 before_fork do |server, worker|
   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-  # old_pid = "#{server.config[:pid]}.oldbin"
-  # if old_pid != server.pid
-  #   begin
-  #     Process.kill "QUIT", File.read(old_pid).to_i
-  #   rescue Errno::ENOENT, Errno::ESRCH
-  #   end
-  # end
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      Process.kill "QUIT", File.read(old_pid).to_i
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
 end
 
 after_fork do |server, worker|
